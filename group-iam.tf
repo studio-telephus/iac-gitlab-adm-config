b08d585733a8d8ee9908c1205b3458b8d0ccb385
Kristo Aun <kristoaun@live.com>
2024-01-15 12:54:11 +0200

    format

--- group-iam.tf
+++ group-iam.tf
@@ -6,17 +6,17 @@ resource "gitlab_group" "iam" {
 }
 
 resource "gitlab_group_variable" "iam_minio_adm_sa_access_key" {
-  group             = gitlab_group.iam.id
-  key               = "MINIO_ADM_SA_ACCESS_KEY"
-  value             = module.minio_sa_api_key_iam.data.username
-  protected         = false
-  masked            = true
+  group     = gitlab_group.iam.id
+  key       = "MINIO_ADM_SA_ACCESS_KEY"
+  value     = module.minio_sa_api_key_iam.data.username
+  protected = false
+  masked    = true
 }
 
 resource "gitlab_group_variable" "iam_minio_adm_sa_secret_key" {
-  group             = gitlab_group.iam.id
-  key               = "MINIO_ADM_SA_SECRET_KEY"
-  value             = module.minio_sa_api_key_iam.data.password
-  protected         = false
-  masked            = true
+  group     = gitlab_group.iam.id
+  key       = "MINIO_ADM_SA_SECRET_KEY"
+  value     = module.minio_sa_api_key_iam.data.password
+  protected = false
+  masked    = true
 }
--- group-platform.tf
+++ group-platform.tf
@@ -6,17 +6,17 @@ resource "gitlab_group" "platform" {
 }
 
 resource "gitlab_group_variable" "platform_minio_adm_sa_access_key" {
-  group             = gitlab_group.platform.id
-  key               = "MINIO_ADM_SA_ACCESS_KEY"
-  value             = module.minio_sa_api_key_platform.data.username
-  protected         = false
-  masked            = true
+  group     = gitlab_group.platform.id
+  key       = "MINIO_ADM_SA_ACCESS_KEY"
+  value     = module.minio_sa_api_key_platform.data.username
+  protected = false
+  masked    = true
 }
 
 resource "gitlab_group_variable" "platform_minio_adm_sa_secret_key" {
-  group             = gitlab_group.platform.id
-  key               = "MINIO_ADM_SA_SECRET_KEY"
-  value             = module.minio_sa_api_key_platform.data.password
-  protected         = false
-  masked            = true
+  group     = gitlab_group.platform.id
+  key       = "MINIO_ADM_SA_SECRET_KEY"
+  value     = module.minio_sa_api_key_platform.data.password
+  protected = false
+  masked    = true
 }
