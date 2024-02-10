b4dd69dd39f5f97f418d987706d0c33db4016ba5
Kristo Aun <kristoaun@live.com>
2024-02-10 03:01:31 +0200

    run

--- group-iam.tf
+++ group-iam.tf
@@ -60,44 +60,7 @@ resource "gitlab_group_variable" "iam_bitwarden_email" {
   raw       = true
 }
 
-
-# Should be split to Gitlab & Spacelift
-resource "gitlab_user" "iam_runner_sa" {
-  name             = "IAM Runner Service Account"
-  username         = module.bw_gitlab_user_iam_runner_sa.data.username
-  password         = module.bw_gitlab_user_iam_runner_sa.data.password
-  email            = "${module.bw_gitlab_user_iam_runner_sa.data.username}@mail.adm.acme.corp"
-  is_admin         = false
-  can_create_group = false
-  is_external      = false
-  reset_password   = false
-}
-
-resource "gitlab_group_membership" "iam_iam_runner_sa" {
-  group_id     = gitlab_group.iam.id
-  user_id      = gitlab_user.iam_runner_sa.id
-  access_level = "maintainer"
-}
-
-resource "gitlab_group_membership" "terraform_iam_runner_sa" {
-  group_id     = gitlab_group.terraform.id
-  user_id      = gitlab_user.iam_runner_sa.id
-  access_level = "reporter"
-}
-
-resource "gitlab_personal_access_token" "iam_runner_sa" {
-  user_id    = gitlab_user.iam_runner_sa.id
-  name       = "GitLab & Spacelift"
-  expires_at = "2025-01-10"
-  scopes     = ["api", "read_api"]
-}
-
-resource "local_sensitive_file" "iam_runner_sa_pat" {
-  content  = gitlab_personal_access_token.iam_runner_sa.token
-  filename = ".terraform/iam_runner_sa_pat.out"
-}
-
-resource "gitlab_project" "iam_group_self_config" {
+resource "gitlab_project" "iac-gitlab-group-iam" {
   name                          = "iac-gitlab-group-iam"
   namespace_id                  = gitlab_group.iam.id
   visibility_level              = "private"
--- group-platform.tf
+++ group-platform.tf
@@ -58,49 +58,7 @@ resource "gitlab_group_variable" "platform_bitwarden_email" {
   raw       = true
 }
 
-
-resource "gitlab_user" "platform_runner_sa" {
-  name             = "Platform Runner Service Account"
-  username         = module.bw_gitlab_user_platform_runner_sa.data.username
-  password         = module.bw_gitlab_user_platform_runner_sa.data.password
-  email            = "${module.bw_gitlab_user_platform_runner_sa.data.username}@mail.adm.acme.corp"
-  is_admin         = false
-  can_create_group = false
-  is_external      = false
-  reset_password   = false
-}
-
-resource "gitlab_group_membership" "platform_platform_runner_sa" {
-  group_id     = gitlab_group.platform.id
-  user_id      = gitlab_user.platform_runner_sa.id
-  access_level = "maintainer"
-}
-
-resource "gitlab_group_membership" "iam_platform_runner_sa" {
-  group_id     = gitlab_group.iam.id
-  user_id      = gitlab_user.platform_runner_sa.id
-  access_level = "reporter"
-}
-
-resource "gitlab_group_membership" "terraform_platform_runner_sa" {
-  group_id     = gitlab_group.terraform.id
-  user_id      = gitlab_user.platform_runner_sa.id
-  access_level = "reporter"
-}
-
-resource "gitlab_personal_access_token" "platform_runner_sa" {
-  user_id    = gitlab_user.platform_runner_sa.id
-  name       = "GitLab"
-  expires_at = "2025-01-10"
-  scopes     = ["api", "read_api"]
-}
-
-resource "local_sensitive_file" "platform_runner_sa_pat" {
-  content  = gitlab_personal_access_token.platform_runner_sa.token
-  filename = ".terraform/platform_runner_sa_pat.out"
-}
-
-resource "gitlab_project" "platform_group_self_config" {
+resource "gitlab_project" "iac-gitlab-group-platform" {
   name                          = "iac-gitlab-group-platform"
   namespace_id                  = gitlab_group.platform.id
   visibility_level              = "private"
